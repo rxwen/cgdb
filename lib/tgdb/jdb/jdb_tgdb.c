abaeb7fe321c2f839eed82a5648fc61857c69799
Raymond Wen <rx.wen218@gmail.com>
2013-08-25 18:39:33 +0800

    implement get_current_location, get_source_file_list, get_source_filepair
    - use 'classpath' command to open a hardcoded file in source view
    - use 'version' command to list all source files

--- lib/tgdb/jdb/jdb_tgdb.c
+++ lib/tgdb/jdb/jdb_tgdb.c
@@ -112,6 +112,7 @@ struct tgdb_jdb {
     pty_pair_ptr pty_pair;
 };
 
+static int in_completion_mode = 0;
 /* initialize_gdbmi
  *
  * initializes a gdbmi subsystem and sets up all initial values.
@@ -243,6 +244,79 @@ static int jdb_get_newline_style(struct ibuf *ibuf, enum newlinestyle *style)
     return 0;
 }
 
+static void update_filepair(struct tgdb_list *list) {
+    struct tgdb_response* response = (struct tgdb_response *)
+            cgdb_malloc(sizeof (struct tgdb_response));
+    response->header = TGDB_FILENAME_PAIR;
+    response->choice.filename_pair.absolute_path = strdup("/home/raymond/.vimrc");
+    response->choice.filename_pair.relative_path = strdup("../../../.vimrc");
+    tgdb_types_append_command(list, response);
+}
+
+static void update_completion(struct tgdb_list *list) {
+    struct tgdb_response *response = (struct tgdb_response *)
+        cgdb_malloc(sizeof (struct tgdb_file_position));
+    static struct tgdb_list *candidates = NULL;
+    if(candidates == NULL) {
+        candidates = tgdb_list_init();
+        tgdb_list_append(candidates, "hello");
+        tgdb_list_append(candidates, "raymond");
+        tgdb_list_append(candidates, "welcome");
+        tgdb_list_append(candidates, "to");
+        tgdb_list_append(candidates, "real");
+        tgdb_list_append(candidates, "word");
+    }
+    response->header = TGDB_UPDATE_COMPLETIONS;
+    response->choice.update_completions.completion_list = candidates;
+    tgdb_types_append_command(list, response);
+}
+
+static void update_location(struct tgdb_list *list) {
+    /* So far, INFO_SOURCE_RELATIVE is only used when a 
+     * TGDB_UPDATE_FILE_POSITION is needed.
+     */
+    /* This section allocates a new structure to add into the queue 
+     * All of its members will need to be freed later.
+     */
+    struct tgdb_file_position *tfp = (struct tgdb_file_position *)
+            cgdb_malloc(sizeof (struct tgdb_file_position));
+    struct tgdb_response *response = (struct tgdb_response *)
+            cgdb_malloc(sizeof (struct tgdb_response));
+
+    tfp->absolute_path = strdup("/home/raymond/projects/cgdb/jdb_test/Test.java");
+    tfp->relative_path = strdup("jdb_test/Test.java");
+    tfp->line_number = atoi("10");
+
+    response->header = TGDB_UPDATE_FILE_POSITION;
+    response->choice.update_file_position.file_position = tfp;
+
+    tgdb_types_append_command(list, response);
+}
+
+static void update_sources(struct tgdb_list* list) {
+    struct tgdb_list *file_list = tgdb_list_init();
+
+    struct tgdb_response *response = (struct tgdb_response *)
+            cgdb_malloc(sizeof (struct tgdb_response));
+
+    static char* sources[] = { "/home/raymond/.vimrc",
+        "/home/raymond/projects/cgdb/jdb_test/com/rmd/Test.java",
+        "/home/raymond/projects/cgdb/jdb_test/Test.java",
+        "/home/raymond/projects/cgdb/jdb_test/cjdb.c"};
+    char* file = NULL;
+    int len = sizeof sources/sizeof sources[0];
+    int i;
+
+    for (i = 0; i < len; ++i) {
+        file = calloc(strlen(sources[i])+1, 1);
+        strcpy(file, sources[i]);
+        tgdb_list_append(file_list, file);
+    }
+    response->header = TGDB_UPDATE_SOURCE_FILES;
+    response->choice.update_source_files.source_files = file_list;
+    tgdb_types_append_command(list, response);
+}
+
 /* 
  * 1. Align the 'input' data into null-terminated strings that
  *    end in a newline.
@@ -259,7 +333,36 @@ int jdb_parse_io(void *ctx,
         char *inferior_output, size_t * inferior_output_size,
         struct tgdb_list *list)
 {
-    return 0;
+    int len = strlen(input_data);
+    int rc = 0;
+    struct tgdb_jdb *jdb = (struct tgdb_jdb *) ctx;
+    if(len >= 2 && 
+            ((input_data[len-2] == ']' && input_data[len-1] == ' ')
+            || (input_data[len-2] == '>' && input_data[len-1] == ' ')))
+            rc = 1;
+
+    strncpy(debugger_output, input_data, len);
+    if('\n' != input_data[len-1]) {
+        debugger_output[len++] = '\n';
+    }
+    debugger_output[len] = '\0';
+    /*strcpy(debugger_output, input_data);//"hello world\n");*/
+    *debugger_output_size = len;//strlen(input_data);
+    if(in_completion_mode == 0)
+        ;
+        /*update_location(list);*/
+        /*update_completion(list);*/
+    else {
+        update_completion(list);
+        in_completion_mode = 0;
+    }
+    if(0 == strncmp("This is jdb version", input_data, strlen("This is jdb version"))) {
+        update_sources(list);
+    }
+    if(0 == strncmp("base directory:", input_data, strlen("base directory:"))) {
+        update_filepair(list);
+    }
+    return rc;
 }
 
 struct tgdb_list *jdb_get_client_commands(void *ctx)
@@ -278,6 +381,9 @@ pid_t jdb_get_debugger_pid(void *ctx)
 
 int jdb_user_ran_command(void *ctx)
 {
+    struct tgdb_jdb *jdb = (struct tgdb_jdb *) ctx;
+    /*if(strncmp(input_data, "update", strlen("update")) == 0)*/
+        /*update_location(jdb);*/
     return 0;
 }
 
@@ -290,7 +396,7 @@ int jdb_get_current_location(void *ctx, int on_startup)
 {
     struct tgdb_jdb *jdb = (struct tgdb_jdb*) ctx;
     struct tgdb_command *client_command = NULL;
-    const char* cmd = "run";
+    const char* cmd = "where\n";
     client_command = tgdb_command_create(cmd, TGDB_COMMAND_TGDB_CLIENT,
     (void *) cmd);
 
@@ -377,17 +483,63 @@ int jdb_is_misc_prompt(void *ctx)
 
 int jdb_completion_callback(void *ctx, const char *command)
 {
+    in_completion_mode = 1;
+    struct tgdb_jdb *jdb = (struct tgdb_jdb*) ctx;
+    struct tgdb_command *client_command = NULL;
+    const char* cmd = "where\n";
+    client_command = tgdb_command_create(cmd, TGDB_COMMAND_TGDB_CLIENT,
+    (void *) cmd);
+
+    tgdb_list_append(jdb->client_command_list, client_command);
     return 0;
 }
 
-int jdb_get_inferior_sources(void *ctx)
-{
+int jdb_get_inferior_sources(void *ctx) {
+    struct tgdb_jdb *jdb = (struct tgdb_jdb*) ctx;
+    struct tgdb_command *client_command = NULL;
+    const char* cmd = "version\n";
+    client_command = tgdb_command_create(cmd, TGDB_COMMAND_TGDB_CLIENT,
+    (void *) cmd);
+
+    tgdb_list_append(jdb->client_command_list, client_command);
     return 0;
 }
 
 char *jdb_return_client_command(void *ctx, enum tgdb_command_type c)
 {
-    return NULL;
+    char *ret = NULL;
+
+    switch (c) {
+        case TGDB_CONTINUE:
+            ret = "continue";
+            break;
+        case TGDB_FINISH:
+            ret = "finish";
+            break;
+        case TGDB_NEXT:
+            ret = "next";
+            break;
+        case TGDB_START:
+            ret = "start";
+            break;
+        case TGDB_RUN:
+            ret = "run";
+            break;
+        case TGDB_KILL:
+            ret = "kill";
+            break;
+        case TGDB_STEP:
+            ret = "step";
+            break;
+        case TGDB_UP:
+            ret = "up";
+            break;
+        case TGDB_DOWN:
+            ret = "down";
+            break;
+    }
+
+    return ret;
 }
 
 char *jdb_client_modify_breakpoint(void *ctx,
